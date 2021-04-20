e5290ffc6ad8479d6887302d0f3412e59ec42146
Juan <apariciojuan30@gmail.com>
2021-04-20 16:58:02 -0300

    changes in services

--- app/services/coinmarketcap/getter_base_service.rb
+++ app/services/coinmarketcap/getter_base_service.rb
@@ -4,13 +4,7 @@ module Coinmarketcap
 
     def initialize(params)
       super()
-      @params = sanitize_params(params)
-    end
-
-    def sanitize_params(params)
-      params[:symbol] = params[:symbol].upcase unless params[:symbol].nil?
-      params[:slug] = params[:slug].downcase unless params[:slug].nil?
-      params
+      @params = params
     end
   end
 end
