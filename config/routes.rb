36b51e3a47ca8b01333bb127c6133b749a3a86d1
Juan <apariciojuan30@gmail.com>
2021-04-19 21:18:36 -0300

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  devise_for :users, controllers: {
+    sessions: 'sessions',
+    registrations: 'registrations',
+    passwords: 'passwords'
+  }
+
+  devise_scope :users do
+    root 'home#index'
+  end
   get 'service-worker.js', to: 'home#service_worker'
 end
