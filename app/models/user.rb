0bd5565d32e1c603dd47d7d05ad5f9bc620701cf
Juan Aparicio <apariciojuan30@gmail.com>
2021-05-20 09:34:05 -0300

    add jumbotron to sign in/up

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,13 @@ class User < ApplicationRecord
 
   has_many :dashboards
   belongs_to :default_coin, class_name: "Coin", foreign_key: "default_coin_id"
+  before_validation :set_default_coin
 
   def default_dashboard
     dashboards.find_by(default: true)
+  end
+
+  def set_default_coin
+    self.default_coin ||= Coin.find_by(symbol: 'USDT')
   end
 end
